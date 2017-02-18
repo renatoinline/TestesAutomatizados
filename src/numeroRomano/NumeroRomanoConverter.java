86e6e36112e0fc97e9affcbfa68de9c397b28e58
Renato Colaco <renatocolaco@gmail.com>
2017-02-17 23:29:04 -0300

    Primeiro commit.
    A casca do projeto contém uma classe que converte
    números romanos e vice-versa e a classe de testes.

---
+++ src/numeroRomano/NumeroRomanoConverter.java
@@ -0,0 +1,15 @@
+package numeroRomano;
+
+public final class NumeroRomanoConverter {
+	
+	private NumeroRomanoConverter(){}
+	
+	public static String toNumeroRomano(int numeroArabico){
+		throw new UnsupportedOperationException();
+	}
+	
+	public static int fromNumeroRomano(String numeroRomano){
+		throw new UnsupportedOperationException();
+	}	
+	
+}
---
+++ src/numeroRomano/Test/NumeroRomanoConverterTest.java
@@ -0,0 +1,19 @@
+package numeroRomano.Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NumeroRomanoConverterTest {
+
+	@Test
+	public void testFromNumeroRomano() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testToNumeroRomano(){
+		fail("Not yet implemented");
+	}
+
+}
