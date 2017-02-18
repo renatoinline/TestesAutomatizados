bfe84ae59315198dab391be1ebc8c7480210658c
Renato Colaco <renatocolaco@gmail.com>
2017-02-18 02:02:43 -0300

    Adicionando um exemplo ao console.

--- src/numeroRomano/Program.java
+++ src/numeroRomano/Program.java
@@ -1,10 +1,24 @@
 package numeroRomano;
 
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		// Exemplo
+		int numeroArabico = 57;
+		
+		System.out.println("Convertendo número para algarismo romano.\n");
+		
+		System.out.println("Convertendo o número: " + numeroArabico);
+		
+		System.out.println("O resultado é: " + NumeroRomanoConverter.toNumeroRomano(numeroArabico));
+		
+		Scanner scanner = new Scanner(System.in); 
+		scanner.nextLine();
+		scanner.close();
 	}
 
 }
